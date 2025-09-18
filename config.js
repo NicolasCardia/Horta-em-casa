67030496e062d43df9a145d9f9034a0bbf980b36
Nicolas Cardia Silva <75455191+NicolasCardia@users.noreply.github.com>
2025-09-17 22:53:58 -0300

    Delete config.js

--- config.js
+++
@@ -1,9 +0,0 @@
-const firebaseConfig = {
-        apiKey: "AIzaSyCVMDKueUNSESOwjg_2x43-Uj3ja19Wl9o",
-        authDomain: "horta-em-casa-app.firebaseapp.com",
-        projectId: "horta-em-casa-app",
-        storageBucket: "horta-em-casa-app.firebasestorage.app",
-        messagingSenderId: "559286368332",
-        appId: "1:559286368332:web:72899b79986af43ef05ce9",
-        measurementId: "G-ED9T88CXKX"
-    };
